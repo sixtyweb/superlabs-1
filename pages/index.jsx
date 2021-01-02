cc28450e138cc70f0b3b528a018075cf3cd24489
sixtyweb <70916417+sixtyweb@users.noreply.github.com>
2021-01-03 00:30:28 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,28 +17,10 @@ export default function Home() {
 
     <div className={styles.projectsContainer}>
     <div className={styles.projectsArticles}>
-    <div className={styles.projectsArticle}>
 
-    
-      <Link href="/projects/bluegame"><img src="bluegame.png"></img></Link>
-    </div>
     <div className={styles.projectsArticle}>
-      <Link href="/projects/eters"><img src="eters.png"></img></Link>
+      <Link href="/projects/ecila"><img src="ecila.png"></img></Link>
     </div>
-    <div className={styles.projectsArticle}>
-      <Link href="/projects/darkland"><img src="darkland.png"></img></Link>
-    </div>
-    <div className={styles.projectsArticle}>
-      <Link href="/projects/newmoon"><img src="newmoon.png"></img></Link>
-    </div>
-    <div className={styles.projectsArticle}>
-      <Link href="/projects/freakminds"><img src="freakminds.png"></img></Link>
-    </div>
-    <div className={styles.projectsArticle}>
-      <Link href="/projects/jaxi"><img src="jaxi.png"></img></Link>
-    </div>
-
-
     </div>
     </div>
     </div>
