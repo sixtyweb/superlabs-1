d511875902f20114cc9ea59e01388cf7b204c778
sixtyweb <70916417+sixtyweb@users.noreply.github.com>
2021-01-02 19:33:15 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ export default function Home() {
   return(
     <>
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>superlabs.</title>
     </Head>
     <div className={styles.container}>
